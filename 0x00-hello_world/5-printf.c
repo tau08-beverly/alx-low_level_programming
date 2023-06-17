c52daf60707a732e169b69b42675e9cac472f084
tau08-beverly@gmail.com <tau.beverly@gmail.com>
2023-06-17 03:08:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 /**
  * Description: main - Print out a string to stdout.
  * Return: 0 if success.
- */int main(void)
+ */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);

b42acaa16e4e86777c124fac0382d943034c4430
tau08-beverly@gmail.com <tau.beverly@gmail.com>
2023-06-21 12:42:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<time.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-
-printf("_putchar\n");
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,3 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#endif

2190ccb8d9c1c113ef6490909cfeafceb8742875
tau08-beverly@gmail.com <tau.beverly@gmail.com>
2023-06-26 11:56:17 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes z pointer to an int as parameter
+ * and update the value it point to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}	

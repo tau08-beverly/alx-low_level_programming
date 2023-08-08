9054ab7532fc6f55d50dd126a2e983f28111f865
tau08-beverly@gmail.com <tau.beverly@gmail.com>
2023-08-08 02:25:52 -0700

    0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to be readed
+ * @text_content: the text inside the file.
+ * Return: 2 success, otherwise 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file - append text to the end of file
+ * @filename: name of file to be readed
+ * @text_content: the text that will be appended.
+ * Return: 2 success, otherwise 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
